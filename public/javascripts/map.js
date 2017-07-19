22f7ea9953e294a34344150e6f3b7cbb8cd792a2
Micaela Linda Villani <villani.micaela@gmail.com>
2017-07-19 16:55:09 +0200

    rendering user positionin the map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,9 +1,43 @@
+var map, infoWindow;
+
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
+       map = new google.maps.Map(document.getElementById('map'), {
           zoom: 13,
           center: {lat: 41.378736, lng: 2.179882}
         });
-        var geocoder = new google.maps.Geocoder();
+
+var geocoder = new google.maps.Geocoder();
+infoWindow = new google.maps.InfoWindow;
+
+
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    infoWindow.open(map);
+    map.setCenter(pos);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+    });
+} else {
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
 
 $.ajax({
        url: "http://localhost:3000/customers/search/json",
@@ -14,7 +48,8 @@ $.ajax({
              lat: chef.location.coordinates[1],
              lng: chef.location.coordinates[0]
            };
-           let marker = new google.maps.Marker({position, map});
+           
+           let marker = new google.maps.Marker({position,map});
 
            let vendorHtml = `<div class="vendor-results row">
             <div class="vendor-img"> </div>
@@ -24,11 +59,13 @@ $.ajax({
             </div>
               <a href="#" class="button get-result"> Show more</a>
           </div>`;
-
+          
+          //show all of the vendors
           $("#vendorList").append(vendorHtml);
 
+          //hide all the vendors on healthy cuisine click
           $(document).on('click', '.healthy-cuisine', function(evt){
-            $("#vendorList").hide(vendorHtml);
+            $("#vendorList").toggle();
           })
 
          });
