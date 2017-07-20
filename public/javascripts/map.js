fc7e89fe66cadfa45ad2856cf58fa3ceb3e037e4
Micaela Linda Villani <villani.micaela@gmail.com>
2017-07-20 10:49:16 +0200

    fixed search yay

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -2,60 +2,66 @@ var map, infoWindow;
 var markersArray = [];
 
 function initMap() {
-       map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: {lat: 41.378736, lng: 2.179882}
-        });
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {
+      lat: 41.378736,
+      lng: 2.179882
+    }
+  });
 
-var geocoder = new google.maps.Geocoder();
-infoWindow = new google.maps.InfoWindow;
+  var geocoder = new google.maps.Geocoder();
+  infoWindow = new google.maps.InfoWindow;
 
 
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    infoWindow.open(map);
-    map.setCenter(pos);
-   
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
     });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
-}
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
-}
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
 
-$.ajax({
-      url: "http://localhost:3000/customers/search/json",
-      type: "get",
-      success: function(response){
-        response.forEach(function(chef, i){
-          let position = {
-            lat: chef.location.coordinates[1],
-            lng: chef.location.coordinates[0]
-          };
-          
+  $.ajax({
+    url: "http://localhost:3000/customers/search/json",
+    type: "get",
+    success: function (response) {
+      response.forEach(function (chef, i) {
+        let position = {
+          lat: chef.location.coordinates[1],
+          lng: chef.location.coordinates[0]
+        };
+
         //  Remove marker
         //  marker.setMap(null);
-        markersArray.push(new google.maps.Marker({position,map}))
+        markersArray.push(new google.maps.Marker({
+          position,
+          map
+        }))
         //let marker = new google.maps.Marker({position,map});
- 
+
         let vendorHtml = `<article id="marker-${i}" class="vendor-results ${chef.cuisine} row">
           <div class="vendor-img"> </div>
           <div class="vendor-description">
@@ -64,42 +70,62 @@ $.ajax({
           </div>
             <a href="#" class="button get-result"> Show more</a>
         </article>`;
-        
+
         //show all of the vendors
         $("#vendorList").append(vendorHtml);
 
         //get the info on click on the page
-    //     google.maps.event.addListener(marker, 'click', function() {
-    //       infoWindow.open(map,marker);
-    // })
-        });
-      },
-       error: function(error){console.log(error)}
-     })
+        //     google.maps.event.addListener(marker, 'click', function() {
+        //       infoWindow.open(map,marker);
+        // })
+      });
+    },
+    error: function (error) {
+      console.log(error)
+    }
+  })
 };
 
 
 //hide all the vendors on healthy cuisine click
-$(document).on('click', '.cuisine-logos', function(evt){
+$(document).on('click', '.cuisine-logos', function (evt) {
 
-  var cuisineName = $(this).attr('id').split('-')[0];                                                                                
-  var chefCards   = $(".vendor-results");
+  var cuisineName = $(this).attr('id').split('-')[0];
+  var chefCards = $(".vendor-results");
   var takeId, idNumber;
 
+  $(this).toggleClass('active')
 
-  chefCards.removeClass('hidden');
-  chefCards.each(function(){
-    if (!$(this).hasClass(cuisineName)) {
+  if ($(this).hasClass('active')) {
+    chefCards.removeClass('hidden');
+    chefCards.each(function () {
+      if (!$(this).hasClass(cuisineName)) {
 
-      takeId = $(this).attr('id');
-      idNumber = takeId.split('marker-')[1];
+        console.log(this);
 
-      console.log('nulling marker # ', idNumber);
+        takeId = $(this).attr('id');
+        idNumber = takeId.split('marker-')[1];
 
-      markersArray[idNumber].setMap(null);
-      $(this).toggleClass('hidden');
-      // markersArray[idNumber].setMap(map);
-      
-    } 
-  });
+        console.log('nulling marker # ', idNumber);
+
+        markersArray[idNumber].setMap(null);
+        $(this).toggleClass('hidden');
+
+        console.log(markersArray);
+        // markersArray[idNumber].setMap(map);
+
+      }
+    });
+  } else {
+    markersArray.forEach(function(marker){
+      marker.setMap(map);
+    })
+
+    chefCards.each(function(card){
+      $(this).removeClass('hidden');
+    })
+  }
+
+
+
 });
