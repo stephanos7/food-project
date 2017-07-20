b63a39ba5fbd7aa8bfab9d16af9328222964a3a9
Micaela Linda Villani <villani.micaela@gmail.com>
2017-07-20 16:53:38 +0200

    fix bug with merging code

--- routes/orders.js
+++ routes/orders.js
@@ -9,28 +9,26 @@ const customerData   = require('connect-mongo') (session);
 const vendorData     = require('connect-mongo') (session);
 
 router.post("/newOrder", (req, res, next) => {
-
   const loggedInUser = req.session.currentCustomer;
-
   // console.log("i am the user in your mind: ", loggedInUser._id);
   // console.log("I am the one who gives food :", req.body.chefId);
   // console.log("I am the food on your plate: ", req.body.orderedItem);
-
   const newOrder = new Order({
     _orderedBy    : loggedInUser._id,
     _orderedFrom  : req.body.chefId,
     _orderItems   : req.body.orderedItem
   });
-
-newOrder.save((err) => {
-  if(err){
-    return next(err);
-  }
-  console.log("saved!");
-})
-
-
-  
+  newOrder.save((err) => {
+    if(err){
+      return next(err);
+    }
+    console.log("saved!");
+  })
 });
+
+router.get("")
+
+
+
 
 module.exports = router;
--- routes/vendors.js
+++ routes/vendors.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const Vendor  = require('../models/vendor');
-const Dish    = require("../models/dish");
-const Order   = require("../models/order");
-const router  = express.Router();
-
+const express        = require('express');
+const Vendor         = require('../models/vendor');
+const Dish           = require("../models/dish");
+const Order          = require("../models/order");
+const router         = express.Router();
 const session        = require("express-session");
 const customerData   = require('connect-mongo') (session);
 const vendorData     = require('connect-mongo') (session);
@@ -22,22 +21,22 @@ router.get("/dashboard", (req, res, next) => {
     const currentUser = req.session.currentVendor;
 
     //query mongo with that id to get the current vendor object
-    Vendor.findById(currentUser._id, (err, theVendorFound) => {
-    if(err){
-        console.log(err);
-    }
-
+    // Vendor.findById(currentUser._id, (err, theVendorFound) => {
+    // if(err){
+    //     console.log(err);
+    // }
+    // return theVendorFound;
 //GET ORDERS ON VENDOR DASHBOARD
     Order.find({ _orderedFrom : currentUser._id} , (err, theOrdersFound) => {
     if(err){
         console.log(err);
     }
-        console.log(theOrdersFound);
+    console.log(theOrdersFound);
+    
     //pass the vendor object to the view to surface it's values
     res.render("vendors/dashboard", {theVendorFound})
     })
     });
-});
 
 //RENDER ADD BIO TO YOUR PROFILE
 router.get("/addbio", (req, res, next) => {
@@ -46,39 +45,15 @@ router.get("/addbio", (req, res, next) => {
 
 //POST NEW BIO TO BE INCLUDED ON YOUR PROFILE
 router.post("/addbio", (req, res, next) => { 
-  
   const currentUser = req.session.currentVendor;
-
   const newBio = {
         about  : req.body.about
-  }
+        }
 
-    Vendor.findByIdAndUpdate(currentUser._id, newBio, (err, theVendorFound) => {
-  if (req.session.currentVendor) { next(); }
-  else { res.redirect("/vendor-login");  }
-});
-
-//GET DASHBOARD 
-router.get("/dashboard", (req, res, next) => {
-    //get the signed-in user’s id from the Session
-    const currentUser = req.session.currentVendor;
-
-   //query mongo with that id to get the current vendor object
-    Vendor.findById(currentUser._id, (err, theVendorFound) => {
-    if(err) {
-        console.log(err);
-    }
-    return theVendorFound;
-      
- //GET ORDERS ON VENDOR DASHBOARD
-    Order.find({ _orderedFrom : theVendorFound} , (err, theOrdersFound) => {
-    if(err){
-        console.log(err);
-    }
-
-    res.redirect("/vendors/dashboard");
-    });
-
+  Vendor.findByIdAndUpdate(currentUser._id, newBio, (err, theVendorFound) => {
+    if (req.session.currentVendor) { next(); }
+    else { res.redirect("/vendor-login");  }
+  })
 });
 
 //RENDER ADD A NEW DISH TO YOUR MENU
@@ -86,28 +61,15 @@ router.get("/newdish", (req, res, next) => {
         res.render("vendors/newdish");
 });
 
-=======
-    console.log(theOrdersFound);
-    
-    //pass the vendor object to the view to surface it’s values
-    res.render("vendors/dashboard", {theVendorFound}, {theOrdersFound});
-    })
-    });
-});
-
-
 //POST NEW DISH TO BE INCLUDED IN THE MENU
 router.post("/newdish", (req, res, next) => { 
-  
   const currentUser = req.session.currentVendor;
-
   const newDish = {
           dishName    : req.body.dishName,
           dishQuantity: req.body.dishQuantity,
           dishPrice   : req.body.dishPrice
   }
-
-    Vendor.findByIdAndUpdate(currentUser._id, {$push: {menu:newDish}}, (err, theVendorFound) => {
+  Vendor.findByIdAndUpdate(currentUser._id, {$push: {menu:newDish}}, (err, theVendorFound) => {
     if(err){
         return next(err);
     }
