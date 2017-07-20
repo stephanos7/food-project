e1296ceae4252d97929363d22803b16f74015cd7
Micaela Linda Villani <villani.micaela@gmail.com>
2017-07-20 11:52:01 +0200

    created order model

--- models/dish.js
+++ models/dish.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const dishSchema = new Schema ({
-    dishName    : String,
+    dishName     : String,
     dishQuantity : Number,
-    dishPrice     : Number,   
+    dishPrice    : Number,   
 });
 
 const Dish = mongoose.model("Dish", dishSchema);
---
+++ models/order.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Customer = require("./customer");
+const orderSchema    = new Schema ({
+    _orderedBy       : { type: Schema.Types.ObjectId, ref: 'Customer'},
+    _orderedFrom     : { type: Schema.Types.ObjectId, ref: 'Vendor'},
+    _orderItems      : [{ type: Schema.Types.ObjectId, ref: 'Dish'}],
+}, {
+    timeStamps : {
+        createdAt : "created_at",
+        updatedAt : "updated_at"
+    
+});
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
