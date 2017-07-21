393fdb2e9a91c554743e9be31a2f2422b11c8b3d
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-21 12:22:22 +0200

    config change, db env variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express                 = require("express");
 const path                    = require("path");
 const logger         = require("morgan");
@@ -25,7 +26,7 @@ const vendorData     = require('connect-mongo') (session);
 
 // Mongoose configuration
 const mongoose       = require("mongoose");
-mongoose.connect("mongodb://localhost/foodApp");
+mongoose.connect(process.env.MONGODB_URI);
 
 // Middlewares configuration
 app.use(logger("dev"));
